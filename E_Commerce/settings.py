98b94bb8b4b0457e4ed22af4ba0857808a932913
Gautam aman <amangautam2128@gmail.com>
2024-01-06 21:53:02 +0530

    Contact page

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -134,3 +134,10 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
 
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="rigel2128@gmail.com "
+EMAIL_HOST_PASSWORD="Aman@2426"
--- home/views.py
+++ home/views.py
@@ -5,12 +5,16 @@ from django.http import HttpRequest, HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.core.mail.message import EmailMessage
+from django.conf import settings
+from django.core import mail 
 
 # Create your views here.
 
 
 
-@login_required(login_url="/login")
+
 def home (request):
     return render(request, "home/templates/home.html")
 
@@ -51,7 +55,7 @@ def register(request):
             return redirect("/login/")
         
     return render(request,'register.html')
-
+@login_required
 def contact(request):
     return render (request, "home/templates/contact.html")
         
@@ -62,7 +66,36 @@ def about(request):
 def test (request):
     return render(request, "home/templates/home1.html")
 
+
+
+@login_required
 def cart(request):
     return render(request, "home/templates/cart.html")
+
+
+def Ccontact(request):
+    if request.method=="POST":
+        data= request.POST
+        fname= data.get('Cname')
+        fmail=data.get('Cemail')
+        desc= data.get('Cmessage')
+        
+        query= contact(name=fname, email=fmail,
+                     description=desc)
+        query.save()
+        from_email=settings.EMAIL_HOST_USER
+        connection=mail.get.connection()
+        connection.open()
+        email_message=mail.EmailMessage(f'Email from {fname}' , f'UserEmail: {fmail}, query:{desc}', from_email,['rigel2128@gmail.com'],
+                                        connection=connection)
+        connection.send_message([email_message])
+        connection.close()
+        return redirect("/contact/")
+    return render(request, "home/templates/contact.html")
+        
+        
+        
+        
+      
 
 
